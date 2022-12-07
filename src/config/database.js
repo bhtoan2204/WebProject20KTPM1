e1736a9ac7d96043f946e3e0ef9f671a0656bf79
Ben Le <ben.l@itcgroup.io>
2022-12-07 21:55:57 +0700

    hotfix postgresql connection

--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,8 @@ const operatorsAliases = require('./operatorAlias');
 
 module.exports = new Sequelize(process.env.POSTGRESQL_URI, {
     dialectOptions: {
-        ssl:true
+        ssl: true,
+        rejectUnauthorized: false
     },
     operatorsAliases: operatorsAliases
 });
