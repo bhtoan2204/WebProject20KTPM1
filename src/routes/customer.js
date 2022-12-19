4fd6079324a5d42e43df644339af4b99cbcf14b5
Ben Le <ben.l@itcgroup.io>
2022-12-19 18:11:56 +0700

    [Feature] - Register

--- src/routes/customer.js
+++ src/routes/customer.js
@@ -8,6 +8,7 @@ const productsController = require('../app/controllers/products.controller');
 const productDetailController = require('../app/controllers/product_detail.controller');
 const userController = require('../app/controllers/user.controller');
 const loginController = require('../app/controllers/login.controller')
+const registerController = require('../app/controllers/register.controller');
 
 router.use('/compair', customerController.compair);
 router.use('/components', customerController.components);
@@ -19,7 +20,7 @@ router.use('/legal_notice', customerController.legal_notice);
 router.get('/login', customerController.login);
 router.post('/login/find', loginController.checkLogin);
 router.get('/logout', customerController.logout);
-router.use('/register', customerController.register);
+router.use('/register', registerController);
 router.use('/normal', customerController.normal);
 router.use('/product_details', productDetailController);
 router.use('/product_summary', customerController.product_summary);
