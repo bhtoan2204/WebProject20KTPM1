65b5b899eaf33324f447ba0887124a892255c8e9
Bành Hảo Toàn <86637323+mjkundta@users.noreply.github.com>
2022-12-19 18:12:50 +0700

    [Feature] - Register (#6)
    Co-authored-by: Ben Le <ben.l@itcgroup.io>

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -52,6 +52,46 @@ const userService = {
                 return reject(error);
             }
         })
+    },
+    checkIfExists: (email) =>{
+        return new Promise(async (resolve, reject) => {
+            try{
+                const user = await User.findOne({
+                    where:{
+                        email:{
+                            $eq:email
+                        }
+                    },
+                    raw:true
+                });
+                return resolve(user);
+            } catch(error){
+                return reject(error)
+            }
+        })
+    },
+    createNewUser: (hashID, newName, newEmail, newPass, newDob, newPhone) => {
+        return new Promise(async (resolve, reject) => {
+            try{
+                const d = new Date();
+                const user = await User.create({
+                    id: hashID, 
+                    name: newName, 
+                    isConfirmed: false, 
+                    dob: newDob, 
+                    phone: newPhone, 
+                    isAdmin: false,
+                    password: newPass,
+                    email: newEmail,
+                    avatarUrl: "https://seud.org/wp-content/uploads/2020/06/avatar-nobody.png",
+                    createAt: d,
+                    updateAt: null
+                });
+                return resolve(user);
+            } catch(error){
+                return reject(error)
+            }
+        })
     }
 }
 
