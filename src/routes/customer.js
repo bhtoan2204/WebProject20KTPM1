7e60ccfc98af3c2b702e8a2cb1f2eb2c7e14603f
Le Tran Gia Bao <70502161+baoletrangia@users.noreply.github.com>
2022-12-25 23:00:51 +0700

    [Enhancement] New layout (#18)
    Co-authored-by: Ben Le <ben.l@itcgroup.io>

--- src/routes/customer.js
+++ src/routes/customer.js
@@ -9,16 +9,17 @@ const productDetailController = require('../app/controllers/product_detail.contr
 const userController = require('../app/controllers/user.controller');
 const loginController = require('../app/controllers/login.controller')
 const registerController = require('../app/controllers/register.controller');
+const cartController = require('../app/controllers/cart.controller');
 
 router.use('/compair', customerController.compair);
 router.use('/components', customerController.components);
 router.use('/contact', customerController.contact);
+router.use('/cart', cartController)
 router.use('/faq', customerController.faq);
 router.use('/forgetpass', customerController.forgetpass);
 router.use('/home', homeController);
 router.use('/legal_notice', customerController.legal_notice);
-router.get('/login', customerController.login);
-router.post('/login/find', loginController.checkLogin);
+router.use('/login', loginController);
 router.get('/logout', customerController.logout);
 router.use('/register', registerController);
 router.use('/normal', customerController.normal);
