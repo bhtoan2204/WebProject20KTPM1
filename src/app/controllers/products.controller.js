ae5c7909c4f92a0be44fb4cbb570f63aaee0ccf0
Bành Hảo Toàn <86637323+mjkundta@users.noreply.github.com>
2022-12-28 18:11:39 +0700

    [Featured] Successful Manage Cart with Ajax

--- src/app/controllers/products.controller.js
+++ src/app/controllers/products.controller.js
@@ -66,47 +66,5 @@ router.get('/search', async (req, res, next) => {
     }
 })
 
-router.post('/add-to-cart', async (req, res) => {
-    try {
-        let message = "";
-        let user = req.cookies["user"];
-        //console.log(user.id)
-        const params = { book_id: req.body.id, quantity: "1" };
-        const book_id_check = params.book_id;
-        //console.log(JSON.stringify(params));
-        const checkExistCart = await cartService.getCart(user.id);
-        //console.log(checkExistCart);
-        if (checkExistCart.length == 0) {
-            const addedCart = await cartService.createNewCart(user.id, JSON.stringify(params));
-            message = "success";
-        }
-        else {
-            // check wheather book is existing in your cart or not
-            var check = true;
-            const listBook = JSON.parse(checkExistCart.products)
-            // console.log(listBook);
-            // console.log(book_id_check);
-            for (var i = 0; i < listBook.length; i++) {
-                if (parseInt(listBook[i].book_id) == book_id_check) {
-                    check = false;
-                    message = "Already in your cart";
-                }
-            }
-            if (check == true) {
-                const productsJson = JSON.parse(checkExistCart.products);
-                productsJson.push(params);
-                //console.log(productsJson);
-                const updatedCart = await cartService.updateCart(user.id, JSON.stringify(productsJson))
-                message = "success"
-            }
-        }
-    }
-    catch (error) {
-        console.log(error);
-        res.status(500).send(error);
-    }
-})
-
-
 
 module.exports = router;
