282bf92ab1a7316bdc6ed11f1f168c6f958cedee
Bành Hảo Toàn <86637323+mjkundta@users.noreply.github.com>
2023-01-04 19:07:21 +0700

    [Feature] Ajax Price Range

--- src/app/controllers/products.controller.js
+++ src/app/controllers/products.controller.js
@@ -5,16 +5,16 @@ const bookService = require('../../services/book.service');
 const qs = require('qs');
 const categoryService = require("../../services/category.service");
 var Paginator = require("paginator");
-const cartService = require("../../services/cart.service");
-const helperService = require("../../services/helper.service");
 const limit = 6;
 
 router.get('/', async (req, res, next) => {
-    let user = req.cookies["user"];
-    const cartQuantity = user ? await cartService.getCartQuantity(user.id) : 0;
     try {
         const { sort: sortFilter } = req.query;
-        const pageAsNum = req.query.page ? Number(req.query.page[1]) : 1;
+        const pageAsNum = req.query.page[1] ? Number(req.query.page[1]) : 1;
+        const from = req.query.from[1] ? Number(req.query.from[1]): 0;
+        const to = req.query.to[1] ? Number(req.query.to[1]): 1000000000;
+
+        console.log(from + "->" + to)
 
         let pageNo = 1
         if (!Number.isNaN(pageAsNum) && pageAsNum > 0) {
@@ -24,27 +24,42 @@ router.get('/', async (req, res, next) => {
         let products = [];
         let pagination_info;
         if (sortFilter === '') {
-            const totalBooks = await bookService.searchBook(req.query);
+            const totalBooks = await bookService.searchBook(req.query, from, to);
             const countBooks = totalBooks.length;
             const paginator = new Paginator(limit, 6);
             pagination_info = paginator.build(countBooks, pageNo);
-            products = helperService.formatBooks(await bookService.searchBookByLimit(req.query, limit * (pageNo - 1), limit));
+            products = await bookService.searchBookByLimit(req.query, limit * (pageNo - 1), limit, from, to);
         }
         else {
-            const totalBooks = await bookService.searchBookAndSorted(req.query);
+            const totalBooks = await bookService.searchBookAndSorted(req.query, from, to);
             const countBooks = totalBooks.length;
             const paginator = new Paginator(limit, 6);
             pagination_info = paginator.build(countBooks, pageNo);
-            products = helperService.formatBooks(await bookService.searchBookAndSortedByLimit(req.query, limit * (pageNo - 1), limit));
+            products = await bookService.searchBookAndSortedByLimit(req.query, limit * (pageNo - 1), limit, from, to);
         }
         const categories = await categoryService.getAllCategories();
         if (pagination_info.total_pages < 2) {
             pagination_info = null;
         }
-        res.render('customer/products', { user, pagination_info, products, categories, cartQuantity });
+        //console.log(products);
+        //console.log(products);
+        let user = req.cookies["user"];
+        res.render('customer/products', { user, pagination_info, products, categories });
     } catch (error) {
         console.log(error);
-        res.render('customer/error500', { user, categories, cartQuantity });
+        res.render('admin/error401')
+    }
+});
+
+router.get('/category/:id', async (req, res, next) => {
+    try {
+        const categoryId = req.params.id;
+        const books = await bookService.getBooksByCategoryId(categoryId);
+        const categories = await categoryService.getAllCategories();
+        res.render('customer/products', { products: books, categories, searchUrl: 'customer/products/search', originalUrl: req.baseUrl, layout: 'customer-main' });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json(error);
     }
 });
 
